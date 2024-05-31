65120890cad2b5d50d6d20eb988c2f2765f48389
AMendoza011003 <131198771+AMendoza011003@users.noreply.github.com>
2024-05-31 17:18:19 -0600

    login barra de navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimengModule } from './primeng.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PrimengModule
+    PrimengModule,
+    BrowserAnimationsModule
   ],
   providers: [
     provideClientHydration()
--- src/app/primeng.module.ts
+++ src/app/primeng.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToolbarModule } from 'primeng/toolbar';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+import { Tooltip, TooltipModule } from 'primeng/tooltip';
+import { MenuModule } from 'primeng/menu';
 
 //Módulos PrimeNg
 const modPrime: any = [
-
+  AvatarModule,
+  ButtonModule,
+  MenubarModule,
+  ToolbarModule,
+  TooltipModule,
+  MenuModule,
 ];
 
 @NgModule({
